4b080c0aa3547620540d1929d84fd799233790d7
Ororophoenix <moniquewill1@yahoo.com>
2015-11-13 18:45:32 -0500

    add week 4 files

---
+++ week-4/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def do_something(height, weight, muscle)
+	puts height
+	puts weight
+	puts muscle
+end
+
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,8 @@
+puts 'What is your full name?'
+name = gets.chomp
+puts 'Hey ' + name + ' its nice to meet you!'
+
+
+puts 'What is your favorite number?'
+number = gets.chomp
+puts 'that number is great but ' + number+'1'.to_s + ' is bigger and better.'
