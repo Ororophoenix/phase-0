8a1d1f056f71ed6b2095e9e5a7079a796e24fae7
Ororophoenix <moniquewill1@yahoo.com>
2015-12-05 13:05:32 -0500

    update git with ne work

---
+++ week-7/eloquent.js
@@ -0,0 +1,30 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+var roller_coaster_review= "Drop Zone sucks"
+  console.log(roller_coaster)
+
+Write a short program that asks for a user to input their favorite food. After they hit return, have the program respond with "Hey! That's my favorite too!" (You will probably need to run this in the Chrome console (Links to an external site.) rather than node since node does not support prompt or alert). Paste your program into the eloquent.js file.
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+ var me = {
+    name: "Monique",
+    age: 24
+    favorite-foods: [pizza, tacos, burgers]
+    quirk: "I'm obsessed with my bucket list"
+  }
---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,16 @@
+//As a user I want to create a function the calculates sum, mean, and median and returns the correct value so that I dont have to create separate functions individually.
+//I want the function to return an error message that explains the error when a incorrect test is inputted.
+
+//As a user I want to create a function for sum
+// The function should be able to calculate the sum of numbers in an odd list
+// The function should be able to calculate the sum of numbers in an even list
+
+//As a user I want to create a function for mean
+// The function should be able to calculate the mean of numbers in an odd list
+// The function should be able to calculate the mean of numbers in an even list
+
+//As a user I want to create a function for median
+// The function should be able to calculate the median of numbers in an odd list
+// The function should be able to calculate the median of numbers in an even list
+
+
